65ec9a899cc9eb062c33d4a3d210777bd6697d20
suesai <suesaiup@qq.com>
2024-08-24 10:54:23 +0800

    treesitter支持更多的语言

--- astronvim/lua/plugins/treesitter.lua
+++ astronvim/lua/plugins/treesitter.lua
@@ -1,15 +1,37 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- Customize Treesitter
-
----@type LazySpec
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
-      "lua",
-      "vim",
-      -- add more arguments for adding more treesitter parsers
-    },
-  },
+	"nvim-treesitter/nvim-treesitter",
+	opts = {
+		ensure_installed = {
+			"asm", "bash", "c", "cmake", "cpp", "css", "diff", "disassembly", "dockerfile", "doxygen", "fish",
+			"git_config", "git_rebase", "gitattributes", "gitcommit", "gitignore", "go", "html", "http",
+			"java", "javascript", "jq", "jsdoc", "json", "json5", "kconfig", "kotlin", "lua", "luadoc",
+			"make", "markdown", "objdump", "perl", "printf", "python", "regex", "rust",
+			"sql", "strace", "systemtap", "toml", "typescript", "vim", "vimdoc", "xml", "yaml",
+		},
+
+		highlight = {
+			enable = true,
+			use_languagetree = true,
+		},
+
+		ignore_install = {
+			"scfg",
+			"smali",
+			"unison",
+		},
+
+		indent = {
+			enable = false,
+		},
+
+		textobjects = {
+			select = {
+			enable = true,
+				include_surrounding_whitespace = false,
+			},
+			swap = {
+				enable = true,
+			}
+		}
+	},
 }
