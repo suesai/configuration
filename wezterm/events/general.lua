5e966d59df97ad1a0212a972f3146088ba869067
suesai <suesaiup@qq.com>
2024-12-24 21:59:51 +0800

    优化wezterm居中

--- wezterm/events/general.lua
+++ wezterm/events/general.lua
@@ -2,24 +2,37 @@ local M = {}
 
 local wez = require("wezterm")
 
-local ratio = 0.7
+local center_window = function(window)
+	local ratio   = 0.7
+	local screens = wez.gui.screens()
+	local active  = screens.active
+	local width, height = active.width * ratio, active.height * ratio
+	local row,   col    = (active.height - height) / 2, (active.width - width) / 2
+
+	if 0 ~= active.x then
+		for key, value in pairs(screens.by_name) do
+			if key ~= active.name then
+				local scale = value.scale / active.scale
+				row = row * scale
+				col = col * scale + value.width
+				break
+			end
+		end
+	end
+
+	window:set_position(col, row)
+	window:set_inner_size(width, height)
+end
 
 M.setup = function()
 	wez.on("gui-startup", function(cmd)
-		local screen = wez.gui.screens().main
-		local width, height = screen.width * ratio, screen.height * ratio
-		local _, _, window = wez.mux.spawn_window(cmd or {
-			position = { x = (screen.width - width) / 2, y = (screen.height - height) / 2 },
-		})
-		window:gui_window():set_inner_size(width, height)
+		local _, _, window = wez.mux.spawn_window(cmd or {})
+		center_window(window:gui_window())
 	end)
 
 	wez.on("gui-attached", function(_)
-		local screen = wez.gui.screens().main
-		local width, height = screen.width * ratio, screen.height * ratio
 		for _, window in ipairs(wez.mux.all_windows()) do
-			window:gui_window():set_position((screen.width - width) / 2, (screen.height - height) / 2)
-			window:gui_window():set_inner_size(width, height)
+			center_window(window:gui_window())
 		end
 	end)
 
@@ -28,10 +41,7 @@ M.setup = function()
 	end)
 
 	wez.on("center_window", function(window)
-		local screen = wez.gui.screens().active
-		local width, height = screen.width * ratio, screen.height * ratio
-		window:set_position((screen.width - width) / 2, (screen.height - height) / 2)
-		window:set_inner_size(width, height)
+		center_window(window)
 	end)
 end
 
