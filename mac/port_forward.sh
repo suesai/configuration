eb8645b928c99782993715c71d1b6b62c9d19d6f
suesai <suesaiup@qq.com>
2025-09-01 08:57:06 +0800

    添加 clash-verge-rev

--- mac/port_forward.sh
+++ mac/port_forward.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-sudo nohup socat TCP4-LISTEN:8800,fork TCP4:127.0.0.1:33210 &>/dev/null &
-# sudo nohup socat TCP4-LISTEN:8801,fork TCP4:127.0.0.1:33211 &>/dev/null &
+sudo nohup socat TCP4-LISTEN:8800,fork TCP4:127.0.0.1:7897 &>/dev/null &
 # sudo nohup socat TCP4-LISTEN:8822,fork TCP4:192.168.15.128:22 &>/dev/null &
