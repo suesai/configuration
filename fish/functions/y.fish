faccf8677ffa1fc8fedeb510ed18946cb7851494
suesai <suesaiup@qq.com>
2025-12-30 11:41:07 +0800

    更新 y 为 yazi 快捷命令，而 yazi 为带返回路径的命令

--- fish/functions/y.fish
+++ fish/functions/y.fish
@@ -1,8 +1,3 @@
-function y --description "yazi with the ability to change the current working directory when exiting"
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	yazi $argv --cwd-file="$tmp"
-	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		builtin cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
+function y --description "yazi"
+	command yazi $argv
 end
---
+++ fish/functions/yazi.fish
@@ -0,0 +1,8 @@
+function yazi --description "yazi with the ability to change the current working directory when exiting"
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	command yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
